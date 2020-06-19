fffb8b199b22d75e3c5cd6e571acd9a776175756
Hugo B Tiburtino <hugotiburtino@protonmail.com>
2020-06-19 20:54:56 +0200

    Add commator function
    This function wraps the functionality of commaspace and justcomma.

--- comma.py
+++ comma.py
@@ -1,33 +1,32 @@
 # comma.py
 
-def justcomma(iterable):
+def commator(iterable, binder=','):
     """
-    Function that takes a iterable and returns a string 
-    with all the items separated by a comma and a space, 
-    with and inserted before the last item. 
+    Mother of other functions like justcomma and commaspace.
+    Takes a iterable and returns a string 
+    with all the items separated by binder parameter
     """
     # raises error if iterable is empty
     if not iterable:
         raise ValueError('The iterable is empty')
 
-    # turn all items of iterable into strings
+    # turns all items of iterable into strings
     string_list = list(map(str, iterable))
     
-    return ','.join(string_list)
+    return binder.join(string_list)
+
+def justcomma(iterable):
+    """
+    Function that takes a iterable and returns a string 
+    with all the items separated by a comma 
+    """
+    return commator(iterable)
 
 def commaspace(iterable):
     """
     Function that takes a iterable and returns a string 
     with all the items separated by a comma and a space.
     """
-
-    # raises error if iterable is empty
-    if not iterable:
-        raise ValueError('The iterable is empty')
-
-    # turn all items of iterable into strings
-    string_list = list(map(str, iterable))
-    
-    return ', '.join(string_list)
+    return commator(iterable, ', ')
 
 
