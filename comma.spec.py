42642c8a33c36eaf61c3d794753931735e1cfafd
Hugo B Tiburtino <hugotiburtino@protonmail.com>
2020-06-19 21:26:20 +0200

    Add docstrings and make the catching of exception more specific

--- comma.spec.py
+++ comma.spec.py
@@ -1,3 +1,7 @@
+"""
+Unit Tests for the comma module
+"""
+
 from comma import commaspace, justcomma, commaand
 
 tuple_spam = ('apples', 'bananas', 'tofu', 'cats')
@@ -5,39 +9,41 @@ list_spam = ['apples', 'bananas', 'tofu', 'cats']
 all_kinds = [0, 'foo', 1.9]
 
 def test_commaspace():
+    "Tests the function commaspace"
     assert commaspace(tuple_spam) == 'apples, bananas, tofu, cats'
     assert commaspace(list_spam) == 'apples, bananas, tofu, cats'
     assert commaspace(all_kinds) == "0, foo, 1.9"
     try:
         commaspace([])
-    except Exception as e:
-        assert type(e) == ValueError
+    except ValueError as exc:
+        assert 'is empty' in str(exc)
     return 'tests passed'
 
 test_commaspace()
 
 def test_justcomma():
+    "Tests the function justcomma"
     assert justcomma(tuple_spam) == 'apples,bananas,tofu,cats'
     assert justcomma(list_spam) == 'apples,bananas,tofu,cats'
     assert justcomma(all_kinds) == "0,foo,1.9"
     try:
         justcomma([])
-    except Exception as e:
-        assert type(e) == ValueError
+    except ValueError as exc:
+        assert 'is empty' in str(exc)
     return 'tests passed'
 
 
 test_justcomma()
 
 def test_commaand():
+    "Tests the function commaand"
     assert commaand(tuple_spam) == 'apples, bananas, tofu, and cats'
     assert commaand(list_spam) == 'apples, bananas, tofu, and cats'
     assert commaand(all_kinds) == "0, foo, and 1.9"
     try:
         commaand([])
-    except Exception as e:
-        assert type(e) == ValueError
+    except ValueError as exc:
+        assert 'is empty' in str(exc)
     return 'tests passed'
-
 
 test_commaand()
